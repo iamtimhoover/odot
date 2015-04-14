beb3d228398f24185caf6fea168d38ef57051683
iamtimhoover <timothyhoov@gmail.com>
2015-04-13 21:37:07 -0400

    Extract todo list spec helpers

--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -9,12 +9,7 @@ describe 'Adding todo items' do
 	
 	let!(:todo_list) { TodoList.create(title: 'Grocery List', description: 'Grocieries') }
 
-	def visit_todo_list(list)
-		visit "/todo_lists"
-		within "#todo_list_#{list.id}" do
-			click_link "List Items"
-		end
-	end
+	
 
 	it 'is successful with valid content' do
 
--- spec/features/todo_items/edit_spec.rb
+++ spec/features/todo_items/edit_spec.rb
@@ -10,15 +10,7 @@ describe 'Editing todo items' do
 	let!(:todo_list) { TodoList.create(title: 'Grocery List', description: 'Grocieries') }
 	let!(:todo_item) { todo_list.todo_items.create(content: "Milk") }
 
-	def visit_todo_list(list)
-		
-		visit "/todo_lists"
-		
-		within "#todo_list_#{list.id}" do
-			click_link "List Items"
-		end
-
-	end
+	
 
 	it 'is successful with valid content' do
 
